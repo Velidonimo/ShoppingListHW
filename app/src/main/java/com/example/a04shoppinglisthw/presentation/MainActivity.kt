482bbd0d5e0bd0a0fc9bc41007a2031db744cbd0
Velidonimo <velidon@mail.ru>
2023-10-11 22:45:27 +0300

    ViewModel done

--- app/src/main/java/com/example/a04shoppinglisthw/presentation/MainActivity.kt
+++ app/src/main/java/com/example/a04shoppinglisthw/presentation/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.a04shoppinglisthw.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.a04shoppinglisthw.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mainViewModel.shopList.observe(this){
+            Log.d("MyMsg", it.toString())
+        }
+
+        mainViewModel.getShopList()
     }
 }
