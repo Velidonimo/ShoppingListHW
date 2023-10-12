37a3fb89f2bbf91a855781807e4289df55b87072
Velidonimo <velidon@mail.ru>
2023-10-12 16:31:56 +0300

    Moved LiveData ohject to UseCase classes

--- app/src/main/java/com/example/a04shoppinglisthw/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/a04shoppinglisthw/presentation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.a04shoppinglisthw.presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.a04shoppinglisthw.data.ShopListRepositoryImpl
 import com.example.a04shoppinglisthw.domain.EditShopItemUseCase
@@ -10,27 +9,19 @@ import com.example.a04shoppinglisthw.domain.ShopItem
 
 class MainViewModel: ViewModel() {
 
-    val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl
 
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
     private val removeShopItemUseCase = RemoveShopItemUseCase(repository)
 
-    val shopList = MutableLiveData<List<ShopItem>>()
-
-
-    fun getShopList(){
-        val list = repository.getShopList()
-        shopList.value = list
-    }
+    val shopList = repository.getShopList()
 
     fun removeShopItem(shopItem: ShopItem){
         repository.removeShopItem(shopItem)
-        getShopList()
     }
 
-    fun editShopItem(shopItem: ShopItem){
+    fun changeEnableStateForShopItem(shopItem: ShopItem){
         repository.editShopItem(shopItem.copy(enabled = !shopItem.enabled))
-        getShopList()
     }
 }
